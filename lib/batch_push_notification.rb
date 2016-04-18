108897d0955686702179afc9fdcb9b030c87ea09
nwittstruck <nwittstruck@users.noreply.github.com>
2016-04-18 12:38:49 +0200

    rename

--- lib/batch_push_notification.rb
+++ lib/batch_push_notification.rb
@@ -1,7 +1,7 @@
 module BatchPushNotification
   autoload :Configurable, 'batch_push_notification/configurable'
   autoload :Client,  'batch_push_notification/client'
-  autoload :Notification,  'batch_push_notification/notification'
+  autoload :Notification, 'batch_push_notification/notification_1'
 
   class << self
     include BatchPushNotification::Configurable
